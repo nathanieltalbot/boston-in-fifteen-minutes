05c60d2c869b47d66b3a09f9f7448c43132bfe99
Nat Talbot <nathanieltalbot@users.noreply.github.com>
2020-04-03 21:04:24 -0400

    Revert "GraphQL now has Public Libraries"
    This reverts commit 763c0a99b73c1eb453e695db4e0cc04480241b2d.

--- gatsby-node.esm.js
+++ gatsby-node.esm.js
@@ -1,7 +1,4 @@
 import {CommuteTimeMidpoint} from './src/lib/util'
-import proj4 from 'proj4'
-import { UTM_STRING, WGS_STRING } from './src/lib/vars'
-
 
 exports.onCreateNode = ({ node, actions }) => {
   const { createNode, createNodeField } = actions
@@ -14,20 +11,6 @@ exports.onCreateNode = ({ node, actions }) => {
           node,
           name: 'averageCommute',
           value: CommuteTimeMidpoint(node)
-      })
-  }
-  
-  else if (node.internal.type === "PublicLibrariesCsv") {
-      var coords = proj4(UTM_STRING, WGS_STRING,[parseFloat(node.X), parseFloat(node.Y)])
-      createNodeField({
-        node,
-        name: 'latitude',
-        value: coords[1]
-      })
-      createNodeField({
-        node,
-        name: 'longitude',
-        value: coords[0]
       })
   }
 }
