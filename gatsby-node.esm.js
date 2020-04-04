08e88d69b5539e9dfd141ff41ca7630cf896dc62
Nat Talbot <nathanieltalbot@users.noreply.github.com>
2020-04-03 22:33:20 -0400

    GraphQL now has public library lat/long

--- gatsby-node.esm.js
+++ gatsby-node.esm.js
@@ -1,16 +1,31 @@
 import {CommuteTimeMidpoint} from './src/lib/util'
+import proj4 from 'proj4'
+import { UTM_STRING, WGS_STRING } from './src/lib/vars'
 
 exports.onCreateNode = ({ node, actions }) => {
   const { createNode, createNodeField } = actions
   // Transform the new node here and create a new node or
   // create a new node field.
-  //console.log(node.internal.type === "HospitalsCsv")
-  //console.log(node.id)
+
   if (node.internal.type === "Acs1216TractCsv") {
       createNodeField({
           node,
           name: 'averageCommute',
           value: CommuteTimeMidpoint(node)
+      })
+  }
+  //Converting UTM into lat/long
+  else if (node.internal.type === "PublicLibrariesCsv") {
+      var coords = proj4(UTM_STRING, WGS_STRING,[parseFloat(node.X), parseFloat(node.Y)])
+      createNodeField({
+        node,
+        name: 'latitude',
+        value: coords[1]
+      })
+      createNodeField({
+        node,
+        name: 'longitude',
+        value: coords[0]
       })
   }
 }
--- src/components/LoadableComponent.js
+++ src/components/LoadableComponent.js
@@ -1,3 +1,3 @@
 import loadable from '@loadable/component'
-const LoadableComponent = loadable(props => import(`./${props.component}`))
+const LoadableComponent = loadable(props => import('./' + props.component))
 export default LoadableComponent
