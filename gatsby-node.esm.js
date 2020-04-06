8c6cd923e8b739bd0cefa853c4f9c17f9da70de7
Nat Talbot <nathanieltalbot@users.noreply.github.com>
2020-04-06 02:02:10 -0400

    Fixing UTM string, hopefully

--- gatsby-node.esm.js
+++ gatsby-node.esm.js
@@ -1,11 +1,14 @@
 import {CommuteTimeMidpoint} from './src/lib/util'
 import proj4 from 'proj4'
-import { UTM_STRING, WGS_STRING } from './src/lib/vars'
+import { UTM_STRING_2, WGS_STRING } from './src/lib/vars'
 
-exports.onCreateNode = ({ node, actions }) => {
+import { Client } from "@googlemaps/google-maps-services-js";
+
+exports.onCreateNode = async ({ node, actions }) => {
   const { createNode, createNodeField } = actions
   // Transform the new node here and create a new node or
   // create a new node field.
+  const new_field = []
 
   if (node.internal.type === "Acs1216TractCsv") {
       createNodeField({
@@ -16,7 +19,7 @@ exports.onCreateNode = ({ node, actions }) => {
   }
   //Converting UTM into lat/long
   else if (node.internal.type === "PublicLibrariesCsv" || node.internal.type === "PublicSchoolsCsv") {
-      var coords = proj4(UTM_STRING, WGS_STRING,[parseFloat(node.X), parseFloat(node.Y)])
+      var coords = proj4(UTM_STRING_2, WGS_STRING,[parseFloat(node.X), parseFloat(node.Y)])
       createNodeField({
         node,
         name: 'latitude',
--- src/lib/vars.js
+++ src/lib/vars.js
@@ -5,6 +5,8 @@ export const FIFTEEN_DIST = 0.8;
 //UTM string for converting Boston UTM coords into latitude and longitude
 export const UTM_STRING = "+proj=lcc +lat_1=42.68333333333333 +lat_2=41.71666666666667 +lat_0=41 +lon_0=-71.5 +x_0=200000 +y_0=750000 +ellps=GRS80 +datum=NAD83 +units=m +no_defs ";
 
+export const UTM_STRING_2 = "+proj=lcc +lat_1=42.68333333333333 +lat_2=41.71666666666667 +lat_0=41 +lon_0=-71.5 +x_0=200000.0001016002 +y_0=750000 +ellps=GRS80 +datum=NAD83 +to_meter=0.3048006096012192 +no_defs "
+
 //Proj4js string for converting into Lat/Long
 export const WGS_STRING = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs";
 
