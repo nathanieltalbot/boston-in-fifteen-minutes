6bd8cbc9c21830969858e64e7a1fc83c1ba2a5d7
Nat Talbot <nathanieltalbot@users.noreply.github.com>
2020-04-20 09:47:41 -0400

    Updated FIFTEEN_DIST and added 15-minute aalysis

--- src/lib/vars.js
+++ src/lib/vars.js
@@ -1,6 +1,6 @@
 
 // Important: The distance traveled by a person on foot in 15 minutes.
-export const FIFTEEN_DIST = 0.8;
+export const FIFTEEN_DIST = 0.75;
 
 //UTM string for converting Boston UTM coords into latitude and longitude
 export const UTM_STRING = "+proj=lcc +lat_1=42.68333333333333 +lat_2=41.71666666666667 +lat_0=41 +lon_0=-71.5 +x_0=200000 +y_0=750000 +ellps=GRS80 +datum=NAD83 +units=m +no_defs ";
