8d729ac162c063fba9cd4c153ae49722a221f0c9
Nat Talbot <nathanieltalbot@users.noreply.github.com>
2020-04-20 00:11:35 -0400

    Added grocery store analysis

---
+++ src/components/GroceryStores.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby'
+import { GeoJSON } from 'react-leaflet'
+import L from 'leaflet'
+import { MeterToMile, utmToLatLng } from '../lib/util.js'
+import ReactDOMServer from 'react-dom/server';
+import { FIFTEEN_DIST } from '../lib/vars.js';
+
+var Rainbow = require('rainbowvis.js');
+
+const PopupContent = (props) => {
+    return(<><b>Closest Grocery Store: </b> {props.store.name}, {props.store.distance.toFixed(2)} miles away</>)
+}
+
+export default function GroceryStores() {
+    const data = useStaticQuery(graphql`
+        query GroceryQuery {
+            allFoodRetailers2016Csv(filter: {prim_type: {eq: "Supermarkets/Other Grocery (Exc Convenience) Strs"}, recordtype: {eq: "Verified"}}) {
+                nodes {
+                    longitude
+                    latitude
+                    name
+                    prim_type
+                }
+            },
+            tractsBostonBariLayer(name: {eq: "Tracts_Boston BARI"}) {
+                features {
+                    geometry {
+                        type
+                        coordinates
+                        centroid {
+                            x
+                            y
+                        }
+                    }
+                    featureFields {
+                        ISD_Nbhd
+                        CT_ID_10
+                    }
+                }
+            }
+        }
+    `)
+
+    data.tractsBostonBariLayer.features.map((feature) => {
+        const nbhd_point = L.latLng(feature.geometry.centroid.y, feature.geometry.centroid.x);
+        const store_list = data.allFoodRetailers2016Csv.nodes.map((store) => {
+            const coords = L.latLng(store.latitude, store.longitude)
+            return({"name": store.name, "distance": MeterToMile(nbhd_point.distanceTo(L.latLng(coords)))})
+        })
+        store_list.sort((a,b) => a.distance - b.distance)
+        feature.closest_store = store_list[0]
+    })
+
+    var gradient = new Rainbow();
+    gradient.setSpectrum('green', 'yellow', 'red')
+    gradient.setNumberRange(FIFTEEN_DIST, 3)
+
+    return(data.tractsBostonBariLayer.features.map((node) => 
+        <GeoJSON data={node.geometry}
+          key={node.featureFields.CT_ID_10}
+          attribution="Boston Data Portal"      
+          color={"#" + gradient.colourAt(node.closest_store.distance)}
+          onEachFeature={( feature, layer ) => layer.bindPopup( ReactDOMServer.renderToString(
+            <PopupContent 
+              store={node.closest_store}
+            />
+          ))}
+          />
+    ))
+}
